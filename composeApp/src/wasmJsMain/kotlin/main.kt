dc7e6d01109e7ceaa6b62314443c1bf0f13660e4
ismail <ismail1973x@gmail.com>
2024-05-12 16:56:37 +0100

    updated

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,15 +1,9 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
-import kotlinx.browser.document
-import kotlinx.browser.window
-import org.w3c.dom.ErrorEvent
-import org.w3c.dom.HTMLDivElement
-import org.w3c.dom.PromiseRejectionEvent
-import org.w3c.dom.events.Event
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    CanvasBasedWindow("SolarSystem") {
+    CanvasBasedWindow("SolarSystemKMP") {
         App()
     }
 }
