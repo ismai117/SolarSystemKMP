36c82cae6c43ad213731359e6fa4e64619fb0658
ismail <ismail1973x@gmail.com>
2024-05-12 13:22:43 +0100

    new web build

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.ksp)
+//    alias(libs.plugins.ksp)
     alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.buildConfig)
 }
