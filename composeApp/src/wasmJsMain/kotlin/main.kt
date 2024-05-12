6f7c69c8c3019d53ebcf783ad302d50850345004
ismail <ismail1973x@gmail.com>
2024-05-12 11:57:49 +0100

    new web build

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,12 +1,34 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
+import kotlinx.browser.document
+import kotlinx.browser.window
+import org.w3c.dom.ErrorEvent
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.PromiseRejectionEvent
+import org.w3c.dom.events.Event
+
+val WasmCompileError: JsAny = js("WebAssembly.CompileError")
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     CanvasBasedWindow(canvasElementId = "ComposeTarget") { App() }
-    if (getPlatform().isMobile){
-        println("its mobile mode")
-    } else {
-        println("its web mode")
+    window.addEventListener("error") {
+        it as ErrorEvent
+        unhandledError(it, it.error!!)
+    }
+    window.addEventListener("unhandledrejection") {
+        it as PromiseRejectionEvent
+        unhandledError(it, it.reason!!)
+    }
+}
+
+
+fun unhandledError(event: Event, error: JsAny) {
+    if (error::class == WasmCompileError) {
+        val warn = document.getElementById("warning") as HTMLDivElement
+        warn.style.display = "initial"
+        // Hide a Scary Webpack Overlay which is less informative in this case.
+        val webPack = document.getElementById("webpack-dev-server-client-overlay") as? HTMLDivElement
+        webPack?.style?.display = "none"
     }
 }
